69d6c988d5956d3b9e68dd3db674eebc84f1889d
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2014-06-04 14:32:19 +0100

    Adds the project skeleton for eclipse development
    * Updates the README
    * Imports scripts from designer
    * Adds the jboss / eclipse luna target platform

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-komodo
-======
+# The Komodo project
 
-teiid modeling tool development utilizing ddl-based metamodel
+## Summary
+
+This is the official git repository for the Komodo project.
+
+Komodo is an open source visual tool that enables rapid, model-driven definition, integration, management and testing of data services without programming using the Teiid runtime framework. With Komodo, not only do you create source data models and map your sources to target formats using a visual tool, but you can also:
+
+*	create a virtual database (or VDB) containing your models which you deploy to a Teiid instance and then access your data.
+*	resolve semantic differences
+*	create virtual data structures at a physical or logical level
+*	use declarative interfaces to integrate, aggregate, and transform the data on its way from source to a target format which is compatible and optimized for consumption by your applications
+
+You can use Komodo to integrate multiple sources, and access them using the common data access standards:
+
+*	Web Services / SOAP / XML
+*	JDBC / SQL
+*	ODBC / SQL
+
+Komodo represents the next generation of modelling tool for Teiid, building on the success of Teiid Designer.
+
+For more information on Komodo, visit the Teiid Designer project's website at [http://www.jboss.org/teiiddesigner/](http://www.jboss.org/teiiddesigner/)
+or follow us on our [blog](http://teiid.blogspot.com/) or on [Twitter](https://twitter.com/teiiddesigner). Or hop into our [IRC chat room](http://www.jboss.org/teiiddesigner/chat)
+and talk our community of contributors and users.
+
---
+++ build.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#################
+#
+# Show help and exit
+#
+#################
+function show_help {
+	echo "Usage: $0 [-d] [-h]"
+	echo "-d - enable maven debugging"
+	echo "-s - skip test execution"
+  exit 1
+}
+
+#
+# This script should be executed from the directory
+# it is located in. Try and stop alternatives
+#
+SCRIPT=`basename "$0"`
+
+if [ ! -f $SCRIPT ]; then
+  echo "This script must be executed from the same directory it is located in"
+  exit 1
+fi
+
+#
+# We must be in the same directory as the script so work
+# out the root directory and find its absolute path
+#
+SCRIPT_DIR=`pwd`
+
+echo "Script directory = $SCRIPT_DIR"
+
+#
+# Set root directory to be its parent since we are downloading
+# lots of stuff and the relative path to the parent pom is
+# ../build/parent/pom.xml
+#
+ROOT_DIR="$SCRIPT_DIR/.."
+
+#
+# By default debug is turned off
+#
+DEBUG=0
+
+#
+# Determine the command line options
+#
+while getopts "bdhs" opt;
+do
+	case $opt in
+	d) DEBUG=1 ;;
+	h) show_help ;;
+  s) SKIP=1 ;;
+	*) show_help ;;
+	esac
+done
+
+#
+# Source directory containing komodo codebase
+# Should be the same directory as the build script location
+#
+SRC_DIR="${SCRIPT_DIR}"
+
+#
+# Maven repository to use.
+# Ensure it only contains komodo related artifacts and
+# does not clutter up user's existing $HOME/.m2 repository
+#
+LOCAL_REPO="${ROOT_DIR}/m2-repository"
+
+#
+# Maven command
+#
+MVN="mvn clean install"
+
+#
+# Turn on dedugging if required
+#
+if [ "${DEBUG}" == "1" ]; then
+  MVN_FLAGS="-e -X -U"
+fi
+
+#
+# Skip tests
+#
+if [ "${SKIP}" == "1" ]; then
+  SKIP_FLAG="-DskipTests"
+fi
+
+#
+# Maven options
+# -P <profiles> : The profiles to be used for downloading jbosstools artifacts
+# -D maven.repo.local : Assign the $LOCAL_REPO as the target repository
+#
+MVN_FLAGS="${MVN_FLAGS} -P target-platform,multiple.target -Dmaven.repo.local=${LOCAL_REPO} -Dno.jbosstools.site -Dtycho.localArtifacts=ignore ${SKIP_FLAG}"
+
+echo "==============="
+
+# Build and test the komodo codebase
+echo "Build and install the komodo plugins"
+cd "${SRC_DIR}"
+echo "Executing ${MVN} ${MVN_FLAGS}"
+${MVN} ${MVN_FLAGS}
