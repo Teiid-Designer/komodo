41c2bf5ddacbcb34e94b97bd186213ff1344cbc8
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2015-01-19 16:49:22 +0000

    Adds Workspace and Schema relational objects
    * WorkspaceManager
     * Provides methods for creating top-level relational objects
    
    * ObjectImpl
     * [gs]etObjectProperty methods for avoiding replicating boiler-plate code
       in [gs]etter methods in relational objects
    
    * komodo.cnd
     * Since workspace is to be flat and not directly observed by users, there
       is no need for any grouping nodes as all nodes can be found using the
       modeshape search API

---
+++ plugins/org.komodo.relational/src/org/komodo/relational/model/internal/SchemaImpl.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+package org.komodo.relational.model.internal;
+
+import org.komodo.core.KomodoLexicon;
+import org.komodo.relational.model.Schema;
+import org.komodo.spi.KException;
+import org.komodo.spi.repository.Property;
+import org.komodo.spi.repository.Repository;
+import org.komodo.spi.repository.Repository.UnitOfWork;
+
+/**
+ * A named schema fragment
+ */
+public class SchemaImpl extends RelationalObjectImpl implements Schema {
+
+    /**
+     * @param repository
+     *        the repository where the relational object exists (cannot be <code>null</code>)
+     * @param workspacePath
+     *        the workspace relative path (cannot be empty)
+     * @throws KException
+     *         if an error occurs
+     */
+    public SchemaImpl( final Repository repository, final String workspacePath ) throws KException {
+        super(repository, workspacePath);
+    }
+
+    @Override
+    public String getRendition(UnitOfWork uow) throws KException {
+        String rendition = getObjectProperty(uow,
+                                       Property.ValueType.STRING,
+                                       "getRendition", //$NON-NLS-1$
+                                       KomodoLexicon.Schema.RENDITION);
+        
+        return rendition == null ? EMPTY_STRING : rendition;
+    }
+
+    @Override
+    public void setRendition(UnitOfWork uow, String rendition) throws KException {
+        setObjectProperty(uow, "setRendition", KomodoLexicon.Schema.RENDITION, rendition); //$NON-NLS-1$
+    }
+
+}
---
+++ plugins/org.komodo.relational/src/org/komodo/relational/workspace/WorkspaceManager.java
@@ -0,0 +1,190 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.  Some portions may be licensed
+ * to Red Hat, Inc. under one or more contributor license agreements.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ */
+package org.komodo.relational.workspace;
+
+import org.komodo.core.KomodoLexicon;
+import org.komodo.relational.model.Model;
+import org.komodo.relational.model.Schema;
+import org.komodo.relational.model.internal.ModelImpl;
+import org.komodo.relational.model.internal.SchemaImpl;
+import org.komodo.spi.KException;
+import org.komodo.spi.repository.KomodoObject;
+import org.komodo.spi.repository.Repository;
+import org.komodo.spi.repository.Repository.Id;
+import org.komodo.spi.repository.Repository.UnitOfWork;
+import org.komodo.spi.utils.KeyInValueHashMap;
+import org.komodo.spi.utils.KeyInValueHashMap.KeyFromValueAdapter;
+import org.komodo.utils.ArgCheck;
+
+/**
+ *
+ */
+public class WorkspaceManager {
+
+    private static class WskpMgrAdapter implements KeyFromValueAdapter<Repository.Id, WorkspaceManager> {
+
+        @Override
+        public Id getKey(WorkspaceManager value) {
+            Repository repository = value.getRepository();
+            return repository.getId();
+        }
+    }
+
+    private static KeyFromValueAdapter<Repository.Id, WorkspaceManager> adapter = new WskpMgrAdapter();
+
+    private static KeyInValueHashMap<Repository.Id, WorkspaceManager> instances = new KeyInValueHashMap<Repository.Id, WorkspaceManager>(adapter);
+
+    private final Repository repository;
+
+    /**
+     * @param repository the repository
+     * @return singleton instance for the given repository
+     */
+    public static WorkspaceManager getInstance(Repository repository) {
+        WorkspaceManager instance = instances.get(repository.getId());
+        if (instance == null) {
+            instance = new WorkspaceManager(repository);
+            instances.add(instance);
+        }
+
+        return instance;
+    }
+
+    private WorkspaceManager(Repository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * @return the repository
+     */
+    public Repository getRepository() {
+        return this.repository;
+    }
+
+    /**
+     * Only one of the {@link UnitOfWork transactions} passed in should be non-<code>null</code>. Ensures that a transaction
+     * rollback occurs if the transaction was constructed within the method.
+     *
+     * @param transactionParameter
+     *        the transaction passed into the method (can be <code>null</code>)
+     * @param transactionVariable
+     *        the transaction constructed within the method (can be <code>null</code>)
+     * @param e
+     *        the error being handled (cannot be <code>null</code>)
+     * @return the error passed in if already a {@link KException} or the error passed in wrapped in a {@link KException}
+     */
+    private static KException handleError( final UnitOfWork transactionParameter,
+                                           final UnitOfWork transactionVariable,
+                                           final Exception e ) {
+        assert (e != null);
+        assert ((transactionParameter == null) && (transactionVariable != null))
+        || ((transactionParameter != null) && (transactionVariable == null));
+
+        if (transactionParameter == null) {
+            transactionVariable.rollback();
+        }
+
+        if (e instanceof KException) {
+            return (KException)e;
+        }
+
+        return new KException(e);
+    }
+
+    /**
+     * @param uow
+     *        the transaction (can be <code>null</code> if update should be automatically committed)
+     * @param parent
+     *        the parent of the model object being created (cannot be <code>null</code>)
+     * @param modelName
+     *        the name of the model to create (cannot be empty)
+     * @return the model object (never <code>null</code>)
+     * @throws KException
+     *         if an error occurs
+     */
+    public Model createModel(UnitOfWork uow, KomodoObject parent, String modelName) throws KException {
+        ArgCheck.isNotNull(parent, "parent"); //$NON-NLS-1$
+        ArgCheck.isNotEmpty(modelName, "modelName"); //$NON-NLS-1$
+
+        UnitOfWork transaction = uow;
+
+        if (uow == null) {
+            transaction = getRepository().createTransaction("workspacemanager-createModel", false, null); //$NON-NLS-1$
+        }
+
+        assert (transaction != null);
+
+        try {
+            final KomodoObject kobject = getRepository().add(transaction,
+                                                        parent.getAbsolutePath(),
+                                                        modelName,
+                                                        KomodoLexicon.VdbModel.NODE_TYPE);
+
+            if (uow == null) {
+                transaction.commit();
+            }
+
+            return new ModelImpl(getRepository(), kobject.getAbsolutePath());
+        } catch (final Exception e) {
+            throw handleError(uow, transaction, e);
+        }
+    }
+
+    /**
+     * @param uow
+     *        the transaction (can be <code>null</code> if update should be automatically committed)
+     * @param parent
+     *        the parent of the schema object being created (cannot be <code>null</code>)
+     * @param schemaName
+     *        the name of the schema to create (cannot be empty)
+     * @return the schema object (never <code>null</code>)
+     * @throws KException
+     *         if an error occurs
+     */
+    public Schema createSchema(UnitOfWork uow, KomodoObject parent, String schemaName) throws KException {
+        ArgCheck.isNotNull(parent, "parent"); //$NON-NLS-1$
+        ArgCheck.isNotEmpty(schemaName, "schemaName"); //$NON-NLS-1$
+
+        UnitOfWork transaction = uow;
+
+        if (uow == null) {
+            transaction = getRepository().createTransaction("workspacemanager-createSchema", false, null); //$NON-NLS-1$
+        }
+
+        assert (transaction != null);
+
+        try {
+            final KomodoObject kobject = getRepository().add(transaction,
+                                                        parent.getAbsolutePath(),
+                                                        schemaName,
+                                                        KomodoLexicon.Schema.NODE_TYPE);
+
+            if (uow == null) {
+                transaction.commit();
+            }
+
+            return new SchemaImpl(getRepository(), kobject.getAbsolutePath());
+        } catch (final Exception e) {
+            throw handleError(uow, transaction, e);
+        }
+    }
+}
